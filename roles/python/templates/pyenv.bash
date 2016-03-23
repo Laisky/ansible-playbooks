c13b755a8ebf2067f91069735ced74e6009348d9
Laisky <ppcelery@gmail.com>
2016-03-23 21:52:29 +0800

    * fix python

--- roles/python/templates/pyenv.bash
+++ roles/python/templates/pyenv.bash
@@ -1,4 +1,4 @@
-export PYENV_ROOT="{{home}}/.pyenv"
+export PYENV_ROOT="{{pyenv}}"
 export PATH="/usr/local/bin:$PYENV_ROOT/bin:$PYENV_ROOT/shims:/usr/texbin:$PATH"
 export PYTHON_BUILD_MIRROR_URL=http://blog.laisky.com/pyenv/pythons/
 eval "$(pyenv init -)"
